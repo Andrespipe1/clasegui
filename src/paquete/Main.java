ca92e0f4de196f156eff335f0bac061c01525beb
POO <andresftma@gmail.com>
2024-06-21 11:45:34 -0500

    as

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,16 +1,26 @@
 package paquete;
 import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         JFrame ventana = new JFrame();
-        ventana.setSize(400,400);
-        ventana.setVisible(true);
+        ventana.setSize(400, 400);
         ventana.setTitle("TITULOOOOS");
+
         ventana.setLayout(new BoxLayout(ventana.getContentPane(),BoxLayout.Y_AXIS));
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ensure the application exits when the window is closed
+
+        JPanel contenedor = new JPanel();
+        contenedor.setBackground(Color.BLUE);
+
         JLabel label = new JLabel("2024-06-21");
-        ventana.getContentPane().add(label);
+        contenedor.add(label);
 
         JTextField campo1 = new JTextField();
-        ventana.getContentPane().add(campo1);
+        contenedor.add(campo1);
+
+        ventana.add(contenedor);
+        ventana.setVisible(true);
     }
 }
